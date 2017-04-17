6a9f2fc0556b79993c0efdb00a37438494f7423b
loveKino <842914439@qq.com>
2017-04-17 15:59:27 +0800

    fix webpack require bug

--- src/buildTestProject.js
+++ src/buildTestProject.js
@@ -3,7 +3,7 @@
 let path = require('path');
 let promisify = require('es6-promisify');
 let fs = require('fs');
-let spawnp = require('spawnp');
+let webpack = require('webpack');
 
 let mkdirp = promisify(require('mkdirp'));
 let readFile = promisify(fs.readFile);
@@ -11,44 +11,49 @@ let writeFile = promisify(fs.writeFile);
 
 const PLAIN_HTML = path.join(__dirname, './pageTpl/plain.html');
 const INDEX_JS = path.join(__dirname, './pageTpl/index.js');
-const WEBPACK_CONFIG_JS = path.join(__dirname, './pageTpl/webpack.config.js');
 
 let buildTestProject = (jsCode, options) => {
     let testDir = options.testDir;
 
     let htmlTplPath = options.htmlTplPath || PLAIN_HTML;
     let indexJsTplPath = options.indexJsTplPath || INDEX_JS;
-    let webpackJsTplPath = options.webpackJsTplPath || WEBPACK_CONFIG_JS;
 
     const testHtmlPath = path.join(testDir, 'index.html');
     const testIndexJsPath = path.join(testDir, 'index.js');
     const testTestJsPath = path.join(testDir, 'test.js');
-    const testWebpackConfigJsPath = path.join(testDir, 'webpack.config.js');
 
     return mkdirp(testDir).then(() => {
         // copy html file, js file
         return Promise.all([
             copyFile(htmlTplPath, testHtmlPath),
-            copyFile(indexJsTplPath, testIndexJsPath),
-            copyFile(webpackJsTplPath, testWebpackConfigJsPath)
+            copyFile(indexJsTplPath, testIndexJsPath)
         ]);
     }).then(() => {
         // create test.js
         return writeFile(testTestJsPath, jsCode, 'utf-8');
     }).then(() => {
-        return spawnp('npm bin', [], null, {
-            stdout: true
-        }).then(({
-            stdouts
-        }) => {
-            let binPath =stdouts.join('\n').trim();
-            // building with webpack
-            return spawnp(`${binPath}/webpack`, [], {
-                cwd: testDir,
-                path,
-                stdio: options.displayPack ? 'inherit' : 'ignore'
-            });
+        let compiler = webpack({
+            entry: {
+                app: [path.join(testDir, './index.js')]
+            },
+            output: {
+                path: path.join(testDir, '/asset'),
+                filename: '[name].js'
+            }
+        });
 
+        return new Promise((resolve, reject) => {
+            try {
+                compiler.run((err, stats) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(stats);
+                    }
+                });
+            } catch (err) {
+                reject(err);
+            }
         });
     });
 };
